a733e7588cc4a37a12fe9a7810971cc3bc76156c
safi-io <m.safi.ullah@outlook.com>
2025-02-09 02:49:47 +0500

    Commiting from Bash

---
+++ src/SixStacksTwo_UsingLL/One.java
@@ -0,0 +1,6 @@
+package SixStacksTwo_UsingLL;
+
+public class One {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/SixStacksTwo_UsingLL/StackLL.java
@@ -0,0 +1,4 @@
+package SixStacksTwo_UsingLL;
+
+public class StackLL {
+}
