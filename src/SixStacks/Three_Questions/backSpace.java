4441f0734e976a61ffccde7595c13c103e763abd
safi-io <m.safi.ullah@outlook.com>
2025-02-11 20:41:44 +0500

    Stack Questions

---
+++ src/SixStacks/Three_Questions/backSpace.java
@@ -0,0 +1,40 @@
+package SixStacks.Three_Questions;
+
+import java.util.Stack;
+
+public class backSpace {
+
+    public static void main(String[] args) {
+        String s = solvedSTR("a##c");
+        String t = solvedSTR("#a#c");
+        //        String t = solvedSTR("ad#c");
+//        System.out.println(s.equals(t));
+        System.out.println(s);
+        System.out.println(t);
+    }
+
+    public static String solvedSTR(String s) {
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (!st.isEmpty() && s.charAt(i) == '#') {
+                st.pop();
+            } else {
+                if (s.charAt(i) != '#') {
+                    st.push(s.charAt(i));
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            if (st.peek() != '#') {
+                sb.append(st.pop());
+            }else {
+                st.pop();
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+}
