d119b81c55bb489563fd7ee5f0c47596e787a8cf
safi-io <m.safi.ullah@outlook.com>
2025-02-23 18:53:18 +0500

    Done with Stacks

--- src/SixStacks/Three_Questions/largestRectangleHistogram.java
+++ src/SixStacks/Three_Questions/largestRectangleHistogram.java
@@ -6,15 +6,24 @@ import java.util.Stack;
 
 public class largestRectangleHistogram {
     public static void main(String[] args) {
-        int[] height = {2, 1, 5, 6, 2, 3};
+        int[] height = {2, 3, 4, 2, 6, 5, 4, 5, 3};
         System.out.println(lrh(height));
     }
 
     private static int lrh(int[] height) {
         int[] nextSmallestRight = nsr(height);
-//        int[] nextSmallestLeft = nsl(height);
-        System.out.println(Arrays.toString(nextSmallestRight));
-        return -1;
+        int[] nextSmallestLeft = nsl(height);
+
+        int rect = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            height[i] = height[i] * (nextSmallestRight[i] - nextSmallestLeft[i] - 1);
+            if (height[i] > rect) {
+                rect = height[i];
+            }
+        }
+
+        return rect;
     }
 
     private static int[] nsr(int[] height) {
@@ -22,25 +31,40 @@ public class largestRectangleHistogram {
         Stack<Integer> st = new Stack<>();
 
         for (int i = 0; i < height.length; i++) {
-            while(!st.isEmpty() && height[st.peek()] > height[i] ) {
+            while (!st.isEmpty() && height[st.peek()] > height[i]) {
                 toReturn[st.peek()] = i;
                 st.pop();
             }
 
-            if(st.isEmpty()) {
-
-            }else {
             st.push(i);
-
-            }
-
         }
 
+        while (!st.isEmpty()) {
+            toReturn[st.pop()] = height.length;
+        }
 
         return toReturn;
     }
 
     private static int[] nsl(int[] height) {
-    return new int[2];
+        int[] toReturn = new int[height.length];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = height.length - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && height[st.peek()] > height[i]) {
+                toReturn[st.peek()] = i;
+                st.pop();
+            }
+
+            st.push(i);
+
+        }
+
+        while (!st.isEmpty()) {
+            toReturn[st.pop()] = -1;
+        }
+
+        return toReturn;
     }
 }
