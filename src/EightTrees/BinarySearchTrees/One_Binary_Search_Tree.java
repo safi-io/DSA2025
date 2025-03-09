4c8050ee00ff834f7c89274d5b8da1398512ac21
safi-io <m.safi.ullah@outlook.com>
2025-03-09 22:42:57 +0500

    Properties of Binary Search Tree

--- src/EightTrees/BinarySearchTrees/One_Binary_Search_Tree.java
+++ src/EightTrees/BinarySearchTrees/One_Binary_Search_Tree.java
@@ -5,6 +5,8 @@ class BinaryTree {
 }
 
 public class One_Binary_Search_Tree {
+    // In-Order Traversal of BST gives a sorted sequence (Left, Root, Right)
+    // Search is O(H)
     public static void main(String[] args) {
 
     }
