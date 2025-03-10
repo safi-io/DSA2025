90585253d9431fe9857a4b3f1d1a472ce4bdc0f3
safi-io <m.safi.ullah@outlook.com>
2025-03-10 21:38:45 +0500

    Class Assignment # 1

---
+++ src/Assignments/One/JobSchedulingFarm.java
@@ -0,0 +1,12 @@
+package Assignments.One;
+
+import java.util.ArrayList;
+
+public class JobSchedulingFarm {
+    public static class Job {
+        private int JobID;
+        private int executionTime;
+    }
+
+
+}
