f0692a36d49df005881674e3c869ab8b5ffff670
safi-io <m.safi.ullah@outlook.com>
2025-03-20 00:47:25 +0500

    Building Binary Search Trees

--- src/EightTrees/BinarySearchTrees/One_Binary_Search_Tree.java
+++ src/EightTrees/BinarySearchTrees/One_Binary_Search_Tree.java
@@ -1,6 +1,47 @@
 package EightTrees.BinarySearchTrees;
 
+import java.util.Scanner;
+
 class BinaryTree {
+
+    private Node root;
+
+    public Node insert(Node root, int data) {
+
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (root.data > data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void inOrderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrderTraversal(root.left);
+        System.out.print(root.data + "->");
+        inOrderTraversal(root.right);
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
 
 }
 
@@ -9,5 +50,15 @@ public class One_Binary_Search_Tree {
     // Search is O(H)
     public static void main(String[] args) {
 
+        int[] numbers = {9,10,4,6,90,34};
+        BinaryTree tree = new BinaryTree();
+        BinaryTree.Node root = null;
+
+        for(int num : numbers) {
+            root = tree.insert(root, num);
+        }
+
+        tree.inOrderTraversal(root);
+        System.out.print("end");
     }
 }
