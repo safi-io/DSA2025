e7c1ad386d869a6a6c0e96a96275dd51172a1ba9
safi-io <m.safi.ullah@outlook.com>
2025-03-29 12:51:14 +0500

    Generated Test Cases for CallCenterManagement

--- src/Assignments/Lab_Two/TestCases.java
+++ src/Assignments/Lab_Two/TestCases.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CallCenterManagementAdditionalTests {
+public class TestCases {
     private callCenterManagement callCenter;
 
     @BeforeEach
