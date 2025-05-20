ec717c1957aef0556cb597a4e27b631ebf06d068
safi-io <m.safi.ullah@outlook.com>
2025-05-21 00:34:00 +0500

    BFS done on Graphs!

--- src/ElevenGraphs/Creation/UndirectedUnweightedGraphAdjacencyList.java
+++ src/ElevenGraphs/Creation/UndirectedUnweightedGraphAdjacencyList.java
@@ -1,18 +1,53 @@
 package ElevenGraphs.Creation;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
-public class UndirectedUnweightedGraphAdjacencyList {
+public class UndirectedUnweightedGraphAdjacencyList { // O(V+E)
     public static void main(String[] args) {
-        int V = 4;
+        int V = 7;
 
         @SuppressWarnings("unchecked") ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
+        BFS(graph, 1);
 
-        // Print 2's Neighbours
-        printNeighbours(graph, 2);
+        /*
+            1 --- 3
+          /       | \
+        0         |  5 --- 6
+         \        | /
+           2 ---  4
+         */
 
+        // 1 0 3, 2, 4 5, 6
+    }
+
+    public static void BFS(ArrayList<Edge>[] graph, int Source) {
+
+        // Add Immediate Neighbours
+
+
+        Queue<Integer> qe = new LinkedList<>();
+        qe.add(Source);
+
+        HashSet<Integer> visited = new HashSet<>(); // For Checking, If it's already in there!
+
+        while (!qe.isEmpty()) {
+            int current = qe.remove();
+
+            if (!visited.contains(current)) {
+                System.out.print(current + " ");
+                visited.add(current);
+
+                // Add all Neighbours of current edge into graph
+                for (Edge e : graph[current]) {
+                    qe.add(e.dest);
+                }
+            }
+        }
     }
 
     public static void printNeighbours(ArrayList<Edge>[] graph, int index) {
@@ -26,17 +61,29 @@ public class UndirectedUnweightedGraphAdjacencyList {
             graph[i] = new ArrayList<>();
         }
 
+        graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 0));
         graph[1].add(new Edge(1, 3));
 
         graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2, 4));
 
         graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+
     }
 
     static class Edge {
