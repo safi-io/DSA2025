0a6ab1083ec1d8f4262d73a27516b3e9162eb511
safi-io <m.safi.ullah@outlook.com>
2025-06-01 22:30:49 +0500

    and DSA sem-subject Last Assignment is finished!

---
+++ src/Assignments/Lab_Four/Edge.java
@@ -0,0 +1,13 @@
+package Assignments.Lab_Four;
+
+public class Edge {
+    public Character src;
+    public Character dest;
+    public int wt;
+
+    public Edge(char src, char dest, int wt) {
+        this.src = src;
+        this.dest = dest;
+        this.wt = wt;
+    }
+}
